19dd4677a4ed372c32e86ddea30242aaca301184
yuukanedu <nejudesuyo@gmail.com>
2017-04-24 06:35:09 +0000

    remake kadai

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417064556) do
+ActiveRecord::Schema.define(version: 20170424053234) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "status"
+    t.string   "title"
   end
 
 end
